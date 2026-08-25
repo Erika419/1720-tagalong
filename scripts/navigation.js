6b328116ca63ee1c75c3ba5e5cf67089aacf392f
11122171@uvu.edu <11122171@uvu.edu>
2026-08-25 14:12:33 -0600

    tried to fix navigation.js error

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,7 +1,7 @@
 
 //Store the selected elements that we are going to use
 const hambutton= document.querySelector('#hamburger')
-const mainnav= document.querySelector('#hamburger')
+const mainnav= document.querySelector('#navbar')
 
 //Toggle the Show Class on and off
 hambutton.addEventListener('click', () =>{
